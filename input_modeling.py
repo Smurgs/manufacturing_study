a12a5d8f39dcf21bb9d478c0e7fb956dc93e00c1
Stefan <Stefan.Murga@hotmail.com>
2019-02-16 15:59:03 -0500

    qq plot

--- input_modeling.py
+++ input_modeling.py
@@ -3,50 +3,68 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 
 
-def hist_plot(data, title, x_label, y_label):
+def hist_plot(data, filename):
     plt.hist(data, int(np.sqrt(len(data))))
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(title)
+    plt.xlabel('Service Time (min)')
+    plt.ylabel('Frequency')
+    plt.title(filename[:-4] + ' Frequency Distribution')
     plt.show()
 
 
-def chi_square_test(observed, expected, ddof, title):
-    results = stats.chisquare(observed, expected, ddof)
-    result = 'confirms' if results.pvalue > 0.05 else 'rejects'
-    print('Conclusion: ' + title + ' ' + result + ' the null hypothesis with a p value of ' + str(results.pvalue))
+def find_exp_params(data):
+    loc, scale = stats.expon.fit(data)
+    print('Loc: %.2f \tScale:%.2f' % (loc, scale))
+    return loc, scale
+
+
+def qqplot(data, dist, filename):
+    stats.probplot(data, dist=dist, plot=plt)
+    plt.title(filename[:-4] + ' Q-Q Plot')
+    plt.show()
+
+
+def chi_square_test(data, dist):
+    # Get frequency and bin edges for observed data
+    frequency, edges = np.histogram(data, int(np.sqrt(len(data))))
+
+    # Determine expected frequency from exp distribution
+    size = sum(frequency)
+    expected_frequency = [(dist.cdf(edges[i + 1]) - dist.cdf(edges[i])) * size for i in range(len(edges)-1)]
+
+    # Group bins together if observed or expected frequency < 5 to conform with chi-square requirements
+    frequency = list(frequency)
+    for i in reversed(range(len(frequency))):
+        if expected_frequency[i] < 5 or frequency[i] < 5:
+            expected_frequency[i - 1] += expected_frequency[i]
+            expected_frequency.pop(i)
+            frequency[i - 1] += frequency[i]
+            frequency.pop(i)
+
+    # Perform chi-square test and print results
+    test_statistic, p_value = stats.chisquare(frequency, expected_frequency, ddof=2)
+    print('Test statistic: %f \tP-value: %f' % (test_statistic, p_value))
+    result = 'confirms' if p_value > 0.05 else 'rejects'
+    print('Conclusion: ' + result + ' the null hypothesis with a p value of ' + str(p_value))
 
 
 if __name__ == '__main__':
 
-    #Content load
-    inspector1_1_data = np.loadtxt('servinsp1.txt')
-    inspector2_2_data = np.loadtxt('servinsp22.txt')
-    inspector2_3_data = np.loadtxt('servinsp23.txt')
-    workstation1_data = np.loadtxt('ws1.txt')
-    workstation2_data = np.loadtxt('ws2.txt')
-    workstation3_data = np.loadtxt('ws3.txt')
+    input_files = ['servinsp1.txt', 'servinsp22.txt', 'servinsp23.txt', 'ws1.txt', 'ws2.txt', 'ws3.txt']
+    for filename in input_files:
 
+        # Load data
+        print('\nLoading file: %s %s' % (filename, '-'*30))
+        data = np.loadtxt(filename)
 
-    #Histogram plots
-    hist_plot(inspector1_1_data, 'Inspector 1 Component 1 Observed Service Times', 'Service Time (min)', 'Frequency')
-    hist_plot(inspector2_2_data, 'Inspector 2 Component 2 Observed Service Times', 'Service Time (min)', 'Frequency')
-    hist_plot(inspector2_3_data, 'Inspector 2 Component 3 Observed Service Times', 'Service Time (min)', 'Frequency')
-    hist_plot(workstation1_data, 'Workstation 1 Observed Service Times', 'Service Time (min)', 'Frequency')
-    hist_plot(workstation2_data, 'Workstation 2 Observed Service Times', 'Service Time (min)', 'Frequency')
-    hist_plot(workstation3_data, 'Workstation 3 Observed Service Times', 'Service Time (min)', 'Frequency')
+        # Plot histogram
+        hist_plot(data, filename)
 
-    # Q-Q Test
-    # a = stats.expon()
-    # e = stats.expon.fit(inspector1_1_data)
-    # print(e)
-    # stats.probplot(inspector1_1_data, dist=stats.expon_gen(), plot=plt)
-    # plt.show()
+        # Fit exponential distribution to data to find parameters
+        loc, scale = find_exp_params(data)
 
-    #chi-square tests
-    chi_square_test((np.histogram(inspector1_1_data))[1], (np.histogram(inspector1_1_data))[1], 0, 'Inspector 1 Service times')
-    chi_square_test((np.histogram(inspector2_2_data))[1], (np.histogram(inspector2_2_data))[1], 0, 'Inspector 2 with Component 2 Service times')
-    chi_square_test((np.histogram(inspector2_3_data))[1], (np.histogram(inspector2_3_data))[1], 0, 'Inspector 2 with Component 3 Service times')
-    chi_square_test((np.histogram(workstation1_data))[1], (np.histogram(workstation1_data))[1], 0, 'Workstation 1 Service times')
-    chi_square_test((np.histogram(workstation2_data))[1], (np.histogram(workstation2_data))[1], 0, 'Workstation 2 Service times')
-    chi_square_test((np.histogram(workstation3_data))[1], (np.histogram(workstation3_data))[1], 0, 'Workstation 3 Service times')
+        # Print qqplot
+        exp_dist = stats.expon(loc=loc, scale=scale)
+        qqplot(data, exp_dist, filename)
+
+        # Chi-square tests
+        chi_square_test(data, exp_dist)
